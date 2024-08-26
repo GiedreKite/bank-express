c39dcc3edf609bcabd95cc5b8454e3c3455f8216
GiedreKite <giedre.narvilaite@gmail.com>
2024-08-27 01:40:12 +0300

    up

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from 'express';
-import { apiRouter } from './router/apiRouter';
+
 
 const app = express();
 const port = 5033;
 
-app.use('/api',apiRouter);
-
-app.get('*', (req, res) => {
-    return res.send('Ups... 404 page 🛸');
-});
 
 app.listen(port, () => {
     console.log(`Serveris pasileido: http://localhost:${port}`);
--- router/apiRauter.js
+++ router/apiRauter.js
@@ -1,3 +1,11 @@
 import express from 'express';
 
 export const apiRouter = express.Router();
+
+apiRouter.get('/', (req, res) => {
+    const data = {
+        state: 'error',
+        message: 'Nurodyk konkretu API endpoint\'a',
+    };
+    return res.json(data);
+});
