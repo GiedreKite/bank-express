9c0baf44f7695dfafc5db86701588f55e01740fa
GiedreKite <giedre.narvilaite@gmail.com>
2024-08-27 09:21:55 +0300

    up

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
-
+import { apiRouter  } from './router/apiRauter.js';
 
 const app = express();
 const port = 5033;
+
+app.use(express.json()) 
+// for parsing application/json
+app.use(express.urlencoded({ extended: true })) 
+// for parsing application/x-www-form-urlencoded
+
+
+app.use('/api',apiRouter);
+
+app.get('*', (req, res) => {
+    return res.send('Ups... 404 page 🛸');
+});
 
 
 app.listen(port, () => {
--- router/apiRauter.js
+++ router/apiRauter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 
+
 export const apiRouter = express.Router();
 
 apiRouter.get('/', (req, res) => {
