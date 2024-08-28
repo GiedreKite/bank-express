29b4dd77d06c5049b78ae3688bb244dabc95d89f
GiedreKite <giedre.narvilaite@gmail.com>
2024-08-28 16:25:35 +0300

    up

--- router/apiRauter.js
+++ router/apiRauter.js
@@ -17,18 +17,53 @@ apiRouter.get('/', (req, res) => {
 const users = [];
 
 apiRouter.get('/account', (req, res) => {
-    const data = {
-        state: 'success',
-        message: 'Jūs užėjote į Giedrės Narvilaitės Banką.',
-    };
-    return res.json(data);
+    return res.json(users);
 });
 
 apiRouter.post('/account', (req, res) => {
     users.push(req.body.account);
+    const name = req.params.name;
+    const surname = req.params.surname;
+    const yearOfBirth = req.params.yearOfBirth;
+    const monthOfBirth = req.params.monthOfBirth;
+    const dayOfBirth = req.params.dayOfBirth;
+   if(name.trim() === ''){
+    return res.json({
+        state: 'error',
+        message: 'Vardas turi būti įrašytas',
+    });
+}
+    if(surname.trim() === ''){
+        return res.json({
+            state: 'error',
+            message: 'Parardė turi būti įrašyta',
+        });
+    }
+    if(yearOfBirth >= 2006){
+        return res.json({
+            state: 'error',
+            message: 'Metai, kuriais gimė, asmuo negali būti sulaukęs pilnametystės',
+        });
+    }
+    if(monthOfBirth >= 8){
+        return res.json({
+            state: 'error',
+            message: 'Mėnuo, kurį gimė, asmuo negali būti sulaukęs pilnametystės',
+        });
+        
+    }
 
+    if(dayOfBirth >= 28){
+        return res.json({
+            state: 'error',
+            message: 'Diena, kurią gimė, asmuo negali būti sulaukęs pilnametystės',
+        });
+        
+    }
     return res.json({
         state: 'success',
         message: 'Vardas, pavardė ir gimimo data pridėta.',
     });
+
 });
+
